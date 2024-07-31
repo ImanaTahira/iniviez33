3e95e5310ff30e3f6ff626e9c204e2d9fd4772e6
ImanaTahira <97746796+ImanaTahira@users.noreply.github.com>
2024-07-31 21:03:59 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+:: Periksa apakah Node.js sudah terinstal
+where node >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Node.js belum terinstal. Silakan instal Node.js dari https://nodejs.org/ dan coba lagi.
+    exit /b 1
+)
+
+:: Navigasi ke direktori skrip
+cd /d "%~dp0"
+
+:: Jalankan skrip
+node maypac.js
+
+pause
